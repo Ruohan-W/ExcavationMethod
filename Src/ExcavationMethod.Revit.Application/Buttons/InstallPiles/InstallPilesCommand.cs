457780ea25c08b2b5d0f66b2f3ddf95c8f16944e
Ruohan Wang <Ruohan.Wang@aecom.com>
2024-04-08 17:35:27 -0600

    add new button for install pill with equal spacing

---
+++ Src/ExcavationMethod.Revit.Application/Buttons/InstallPiles/InstallPilesCommand.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Interop;
+using System.Diagnostics;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+using System.Reflection;
+using ExcavationMethod.Revit.Utilities;
+using ExcavationMethod.Revit.Application.Resources;
+
+namespace ExcavationMethod.Revit.Application.Buttons.InstallPiles
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.NoCommandData)]
+    public class InstallPilesCommand : IExternalCommand
+    {
+        readonly string ButtonName = "Install Piles";
+        readonly string ButtonDescription = "Install Piles with equal spacing along the reference";
+        readonly string ButtonIcon = IconsPackX32.Placeholder;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                return Result.Failed;
+            }
+        }
+
+        public static void CreateButton(RibbonPanel panel)
+        {
+            InstallPilesCommand command = new InstallPilesCommand();
+            string buttonName = command.ButtonName;
+            string buttonDescription = command.ButtonDescription;
+            string buttonIcon = command.ButtonIcon;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string? methodName = MethodBase.GetCurrentMethod().DeclaringType?.Name;
+            string? methodFullName = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+
+            if (methodName != null && methodFullName != null)
+            {
+                PushButtonData button = RibbonUtils.FillPushButtonData(assembly, methodName, buttonName, methodFullName, buttonDescription, buttonIcon);
+                panel.AddItem(button);
+            }
+        }
+    }
+}
